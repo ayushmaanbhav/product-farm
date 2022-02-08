af1a3b871a9421c245e1110e1ec268f0258ed693
Ayush Jain <ayush.jain@navi.com>
2022-02-08 10:52:21 +0530

    glitch integ

--- manager/src/main/kotlin/com/navi/insurance/productFarm/api/HelloWorld.kt
+++ manager/src/main/kotlin/com/navi/insurance/productFarm/api/HelloWorld.kt
@@ -1,12 +1,35 @@
 package com.navi.insurance.productFarm.api
 
+import io.sentry.Sentry
+import org.apache.logging.log4j.LogManager
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api")
 class HelloWorld {
+    
     @GetMapping
     fun findAll() = "Hello World!"
+    
+    @PostMapping
+    fun findAllException() {
+        try {
+            throw RuntimeException("Hello Exception")
+        } catch (e: Exception) {
+            Sentry.captureException(e)
+        }
+        
+        try {
+            throw RuntimeException("Hello Exception Log")
+        } catch (e: Exception) {
+            log.error("Exception 2", e)
+        }
+    }
+    
+    companion object {
+        private val log = LogManager.getLogger()
+    }
 }
